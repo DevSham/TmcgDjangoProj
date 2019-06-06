aa2c676bda3b9abef86f721970e135491c0eaee9
shamim2019 <kjingo12@gmail.com>
2019-06-06 03:10:55 -0400

    connnect psql, add gitignore file

--- apiapp/models.py
+++ apiapp/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 # Create your models here.
 class Posts(models.Model):
-    id = models.IntegerField(primary_key=True)
+    #id = models.IntegerField(primary_key=True, null=False)
     fname = models.CharField(max_length = 100)
     sname = models.CharField(max_length = 100)
     sex = models.CharField(max_length = 100)
