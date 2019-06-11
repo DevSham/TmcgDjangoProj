fd519841d1f418dcfa801b25e7373f5be1dd03be
shamim2019 <kjingo12@gmail.com>
2019-06-11 16:29:54 -0400

    crispy forms

--- ApiProject/settings.py
+++ ApiProject/settings.py
@@ -54,7 +54,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
